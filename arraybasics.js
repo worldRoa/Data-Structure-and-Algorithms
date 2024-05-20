6e070c8f87ffac1fee39cad00fb8f943f98b2201
worldRoa <ravonrolle@gmail.com>
2024-05-20 13:56:06 -0400

    array practice

--- arraybasics.js
+++ arraybasics.js
@@ -53,32 +53,37 @@ const char = [
 // //***REDUCE***
 // //1. Get total mass of all characters
 
-const totalMassChar = char.reduce(
-    (acc, cur) => acc + cur.mass, 0)
+// const totalMassChar = char.reduce(
+//     (acc, cur) => acc + cur.mass, 0)
 
-    console.log(totalMassChar);
+//     console.log(totalMassChar);
 
-// //2. Get total height of all characters
+// // //2. Get total height of all characters
 
-const totalHeight = char.reduce(
-    (acc, cur) => acc + cur.height, 0)
+// const totalHeight = char.reduce(
+//     (acc, cur) => acc + cur.height, 0)
 
-    console.log(totalHeight);
-// //3. Get total number of characters by eye color
+//     console.log(totalHeight);
+// // //3. Get total number of characters by eye color
 
-const totalNumCharByEyeColor = char.reduce((acc, cur) => {
-    const color = cur.eye_color;
-    if(acc[color]) {
-        acc[color]++;
-    }else {
-        acc[color] = 1;
-    }
-    return acc;
-}, {});
+// const totalNumCharByEyeColor = char.reduce((acc, cur) => {
+//     const color = cur.eye_color;
+//     if(acc[color]) {
+//         acc[color]++;
+//     }else {
+//         acc[color] = 1;
+//     }
+//     return acc;
+// }, {});
 
-console.log(totalNumCharByEyeColor);
-// //4. Get total number of characters in all the character names
+// console.log(totalNumCharByEyeColor);
+// // //4. Get total number of characters in all the character names
 
+// const totalNameMum = char.reduce((acc, cur) =>
+
+//     acc + cur.name.length, 0)
+
+// console.log(totalNameMum);
 // //***FILTER***
 // //1. Get characters with mass greater than 100
 
@@ -108,40 +113,84 @@ console.log(totalNumCharByEyeColor);
 
 //***SORT***
 //1. Sort by mass
+
+const CharbyMass = char.sort((a, b) => b.mass - a.mass);
+
+console.log(CharbyMass);
+
 //2. Sort by height
+
+const byHeight = char.sort((a, b) => a.height - b.height);
+
+console.log(byHeight);
+
 //3. Sort by name
+
+const byName = char.sort((a, b) => {
+    if(a.name < b.name) return -1;
+    return 1;
+})
+
+console.log(byName);
+
 //4. Sort by gender
 
+const byGender = char.sort((a, b) => {
+    if(a.gender === 'female') return -1;
+    return 1;
+})
+
+console.log(byGender);
+
 //***EVERY***
-//1. Does every character have blue eyes?
+// //1. Does every character have blue eyes?
 
-const allBlueEyesChar = char.every((char) => char.eye_color === 'blue'
-);
-console.log(allBlueEyesChar);
+// const allBlueEyesChar = char.every((char) => char.eye_color === 'blue'
+// );
+// console.log(allBlueEyesChar);
 
-//2. Does every character have mass more than 40?
+// //2. Does every character have mass more than 40?
 
-const over40MassChar = char.every((char) =>
-    char.mass > 40
-)
-console.log(over40MassChar)
-//3. Is every character shorter than 200?
+// const over40MassChar = char.every((char) =>
+//     char.mass > 40
+// )
+// console.log(over40MassChar)
+// //3. Is every character shorter than 200?
 
-const shortChar = char.every((char) =>
-    char.height < 200);
+// const shortChar = char.every((char) =>
+//     char.height < 200);
 
-console.log(shortChar);
+// console.log(shortChar);
 
-//4. Is every character male?
+// //4. Is every character male?
 
-const maleChar = char.every((char) => char.gender === 'male');
+// const maleChar = char.every((char) => char.gender === 'male');
 
-console.log(maleChar);
+// console.log(maleChar);
 
 //***SOME***
 //1. Is there at least one male character?
 
+// const oneMaleChar = char.some((char) =>
+//     char.gender === 'male');
 
-//2. Is there at least one character with blue eyes?
-//3. Is there at least one character taller than 210?
-//4. Is there at least one character that has mass less than 50?
+// console.log(oneMaleChar);
+
+// //2. Is there at least one character with blue eyes?
+
+// const blueEyesChar = char.some((char) =>
+//     char.eye_color === 'blue');
+
+// console.log(blueEyesChar);
+// //3. Is there at least one character taller than 210?
+
+// const Taller210Char = char.some((char) =>
+//     char.height > 210);
+
+// console.log(Taller210Char);
+// //4. Is there at least one character that has mass less than 50?
+
+// const mass50Char = char.some((char) =>
+//     char.mass < 50)
+
+// console.log(mass50Char);
